5cd7ac959f17361ee2f3133e9baa9ddf0ba12261
matias <mfusco@fi.uba.ar>
2022-09-29 17:30:40 -0300

    Added login screen

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+
+function alertData(email, password) {
+  Alert.alert("Email: " + email + "\n" + "Password: " + password);
+}
 
 export default function App() {
+  const [email, onChangeEmail] = useState(null);
+  const [password, onChangePassword] = useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Welcome to FI-UBER</Text>
+      <Text style={styles.text}>
+        Please enter your email address and password
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+        placeholder="Email"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        value={password}
+        placeholder="Password"
+        secureTextEntry={true}
+        autoCapitalize="none"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => alertData(email, password)}
+      >
+        <Text style={styles.buttonText}>Sign in</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: "dodgerblue"}]}
+        onPress={() => Alert.alert("Register button")}
+      >
+        <Text style={styles.buttonText}>Sign up</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +56,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    justifyContent: "center",
   },
+  title: {
+    color: "white",
+    textAlign: "center",
+    marginBottom: 30,
+    fontSize: 30,
+  },
+  text: { color: "white", textAlign: "center", marginBottom: 15, fontSize: 13 },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    marginTop: 7,
+    marginBottom: 7,
+    backgroundColor: "lightgray",
+    borderRadius: 8,
+  },
+  button: {
+    justifyContent: "space-around",
+    backgroundColor: "lightskyblue",
+    padding: 10,
+    marginTop: 10,
+    borderColor: "dodgerblue",
+    borderRadius: 8,
+    alignItems: "center",
+    margin: 12,
+  },
+  buttonText: {
+    fontSize: 15,
+  }
 });
