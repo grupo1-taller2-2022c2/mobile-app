6d0d916361ab9bc9dc49bbd09668716524ad5a78
matias <mfusco@fi.uba.ar>
2022-10-15 15:14:41 -0300

    Add destination input field on map screen

--- src/Screens/Map.js
+++ src/Screens/Map.js
@@ -2,25 +2,27 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 import * as Location from "expo-location";
 import MapView from "react-native-maps";
+
 import {
   StyleSheet,
   Text,
   View,
   Dimensions,
+  TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 
 export default function Map({ navigation }) {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [destination, onChangeDestination] = useState(null);
 
- 
   useEffect(() => {
     (async () => {
-      
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
         return;
       }
 
@@ -29,30 +31,48 @@ export default function Map({ navigation }) {
     })();
   }, []);
   return errorMsg ? (
-    <View style={styles.container}>
+    <View style={map_styles.container}>
       <Text>{errorMsg}</Text>
       <Text>Give Location permissions to access map</Text>
     </View>
-  ) 
-  : location? (
+  ) : location ? (
     <View>
+      <View style={{ margin: 50, marginBottom: 10 }}>
+        <Text style={map_styles.title}>Please enter your destination</Text>
+        <View style={{flexDirection: 'row'}}>
+          <TextInput
+            style={map_styles.input}
+            onChangeText={onChangeDestination}
+            value={destination}
+            placeholder="Destination..."
+            autoCapitalize="none"
+          />
+          <TouchableOpacity style={map_styles.button} onPress={() => {
+            Alert.alert("Destination set to " + destination);
+          }}>
+          <Text style={map_styles.buttonText}>Go!</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
       <MapView
-        style={styles.map}
+        style={map_styles.map}
         initialRegion={{
           latitude: location.coords.latitude,
           longitude: location.coords.longitude,
           latitudeDelta: 0.01,
           longitudeDelta: 0.01,
         }}
+        showsUserLocation={true}
       />
     </View>
-  )
-  : (<View style={styles.container}>
-    <Text>Loading...</Text>
-  </View>);
+  ) : (
+    <View style={map_styles.container}>
+      <Text>Loading...</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
+const map_styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
@@ -62,5 +82,33 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  title: {
+    color: "black",
+    //textAlign: "center",
+    marginBottom: 10,
+    fontSize: 25,
+  },
+  button: {
+    justifyContent: "space-around",
+    backgroundColor: "lightskyblue",
+    padding: 10,
+    borderColor: "dodgerblue",
+    borderRadius: 8,
+    alignItems: "center",
+    margin: 12,
+    marginLeft: 0,
+
+  },
+  buttonText: {
+    fontSize: 15,
+  },
+  input: {
+    margin: 12,
+    marginLeft: 0,
+    padding: 10,
+    backgroundColor: "lightgray",
+    borderRadius: 8,
+    flex:1,
   },
 });
