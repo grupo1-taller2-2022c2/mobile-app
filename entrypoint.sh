b8e914946cccd924e858ea42df75551e0151605d
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-10-31 01:53:46 -0300

    se saca el --web de expo start en entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $REACT_APP_API_GATEWAY_HOST
 nohup ./launch_ngrok.sh > /dev/null 2>&1 &
-REACT_APP_API_GATEWAY_HOST=$REACT_APP_API_GATEWAY_HOST expo start --tunnel --web
-# REACT_APP_API_GATEWAY_HOST=$REACT_APP_API_GATEWAY_HOST expo start --web
+REACT_APP_API_GATEWAY_HOST=$REACT_APP_API_GATEWAY_HOST expo start --tunnel
+# REACT_APP_API_GATEWAY_HOST=$REACT_APP_API_GATEWAY_HOST expo start
