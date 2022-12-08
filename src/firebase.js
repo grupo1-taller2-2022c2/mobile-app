2bb7ae0bd1868a40dd9c5f20bf5b5a0430f5d1f1
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-12-07 23:08:22 -0300

    Removed comments

--- src/firebase.js
+++ src/firebase.js
@@ -10,32 +10,7 @@ const firebaseConfig = {
   appId: "1:82078037515:web:897d1fda4499bccfddf90e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 export {auth}
-
-// import * as firebase from "firebase";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD-rM_gE33XSgD002OlFmBSFhbaXUkuexY",
-//   authDomain: "fi-uber-auth.firebaseapp.com",
-//   projectId: "fi-uber-auth",
-//   storageBucket: "fi-uber-auth.appspot.com",
-//   messagingSenderId: "82078037515",
-//   appId: "1:82078037515:web:897d1fda4499bccfddf90e"
-// };
-
-
-// let app;
-// if(firebase.apps.length === 0){
-//   app = firebase.initializeApp(firebaseConfig);
-// } else {
-//   app = firebase.app()
-// }
-
-// const auth = firebase.auth()
-
-// export { auth }
