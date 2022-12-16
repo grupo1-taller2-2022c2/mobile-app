9b191dae444e49b1a1efa5ac90ba808dee2333d9
Cristóbal <csabella@fi.uba.ar>
2022-12-16 13:36:21 -0300

    Ahora trips esta estetico!

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -4,22 +4,23 @@ import * as React from "react";
 
 export default function ProfileCard(props) {
     const user = props.data.data;
-    return (
+    return ( user?
         <>
-            <Image source={{uri: user.photo + "?time=" + new Date()}} style={{height: 200, width:200, resizeMode: "contain", borderRadius:5}}/>
+            <Image source={{uri: user.photo + (!props.notUpdate? "?time=" + new Date() : "")}} style={{height: 200, width:200, resizeMode: "contain", borderRadius:5}}/>
             <View style={{marginTop: 20, alignItems:'center'}}>
-                <Text style={{ color: "#fff", fontSize: 40, fontWeight:'bold' }}>{user.username} {user.surname}</Text>
-                <Text style={{ color: "#919090", fontSize: 24}}>{user.email}</Text>
+                <Text style={{ color: props.modal? '#000' : "#fff", fontSize: 40, fontWeight:'bold' }}>{user.username} {user.surname}</Text>
+                <Text style={{ color: props.modal? '#b7b7b7' : "#919090", fontSize: 24}}>{user.email}</Text>
                 {props.isDriver ?
                     <>
-                        <Text style={{ color: "#d2d2d2", fontSize: 30, marginTop: 20 }}>Driving a {user.model}</Text>
-                        <Text style={{ color: "#fff", fontSize: 30, marginTop:20, marginBottom:20,
+                        <Text style={{ color: props.modal? "#919090": "#d2d2d2", fontSize: 30, marginTop: 20 }}>Driving a {user.model}</Text>
+                        <Text style={{ color: props.modal? '#000': "#fff", fontSize: 30, marginTop:20, marginBottom:20,
                             height:70, width: 250, borderRadius:10, borderWidth:2, borderColor:'grey',
                             textAlignVertical:'center', textAlign:'center'}}>
                             {user.licence_plate}
                         </Text>
                     </> : null}
-                <StarRating style={{marginTop: 20}} rating={user.ratings} starSize={25} onChange={() => null} enableSwiping={false} />
+                <StarRating style={{marginTop: props.modal? 0:20}} rating={user.ratings} starSize={props.modal? 40:25} onChange={() => null} enableSwiping={false} />
             </View>
         </>
+            : null
 )}
